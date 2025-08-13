dcdd4f251dad78a873174c73cf758f397386ec98
junny97 <pcohad12@gmail.com>
2025-08-13 22:17:56 +0900

    chore: TanstacRouter 설치, 기존 react-router 삭제 및 라우터 설정 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router-dom';
-import router from './Routes.tsx';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
     <ReactQueryDevtools initialIsOpen={false} />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
